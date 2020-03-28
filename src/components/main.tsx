bed2e0317b67109dd99ba042480a0870d1c9baa9
Ian Phipps <phippsian@gmail.com>
2020-03-28 22:45:18 +0000

    Add components

---
+++ src/components/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as Data from '../../resume.json';
+
+export const Main = (Data) => {
+  console.log(Data);
+  return <main>hello</main>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Header } from '~/components/header';
+import { Body } from '~/components/header';
 
 ReactDOM.render(<>react code goes here</>, document.getElementById('body'));
