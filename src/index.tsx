65a159e25b9e108ec7502a7b88e94ce28ec93596
Ian Phipps <phippsian@gmail.com>
2025-09-07 09:46:23 -0400

    tsc

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { sample } from './sample-resume';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { sample } from "./sample-resume";
 // TODO figure out a better way to store my resume file and not commit it
 // to this repo. Right now, I put my resume at ./re and just don't commit it.
-import { re } from './re';
-import { Header } from './components/header';
-import { Main } from './components/main';
-import { Global } from '@emotion/core';
-import { styles } from './globalStyles';
-import { IResume } from '~lib';
+import { Header } from "./components/header";
+import { Main } from "./components/main";
+import { Global } from "@emotion/core";
+import { styles } from "./globalStyles";
+import { IResume } from "~lib";
 
 const Switcher = () => {
-  const [selected, select] = React.useState<IResume>(re);
+  const [selected, select] = React.useState<IResume>(sample);
   return (
     <div contentEditable spellCheck>
       <div className="no-print">
-        <button onClick={() => select(re)}>re</button>
         <button onClick={() => select(sample)}>sample</button>
       </div>
       <Global styles={styles} />
@@ -25,4 +23,4 @@ const Switcher = () => {
   );
 };
 
-ReactDOM.render(<Switcher />, document.getElementById('body'));
+ReactDOM.render(<Switcher />, document.getElementById("body"));
